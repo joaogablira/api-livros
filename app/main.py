617126da6dd4ed99838e0ff4bc323d07407f18a2
joaogablira <joaogab.lira24@gmail.com>
2026-09-24 07:54:11 -0300

    feat: permite acesso do frontend

--- app/main.py
+++ app/main.py
@@ -6,6 +6,27 @@ from app.database import BaseBanco, mecanismo_banco, obter_sessao_banco
 from app.models import Livro
 from app.schemas import LivroCriacao, LivroResposta
 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="API de Livros",
+    version="1.0.0",
+    description="API didática para gerenciamento de livros.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=False,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type"],
+)
+
 
 BaseBanco.metadata.create_all(bind=mecanismo_banco)
 
